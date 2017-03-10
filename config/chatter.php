9e803f21ec6d268af14fae150ebf487e6c80258e
Chris Breuer <chrisbreuer93@gmail.com>
2017-03-10 11:08:24 -0800

    add plural version of "discussion" in config

--- config/chatter.php
+++ config/chatter.php
@@ -28,11 +28,13 @@ return [
     |
     | These are some default titles (words) that will be used throughout your
     | forum. You can change these to whatever you would like :)
+    | 'discussions' is just the plural version of 'discussion'
     |
     */
 
     'titles' => [
         'discussion' => 'Discussion',
+        'discussions' => 'Discussions',
         'category'   => 'Category',
     ],
 
