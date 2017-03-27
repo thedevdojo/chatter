324cd057d335ba771a1fc7c0db230fe73b6052d7
insoutt <mi@opmbx.org>
2017-03-26 19:11:57 -0500

    Add spanish translations

---
+++ src/Lang/es/messages.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    'words' => [
+        'cancel'  => 'Cancelar',
+        'delete'  => 'Borrar',
+        'edit'    => 'Editar',
+        'yes'     => 'Si',
+        'no'      => 'No',
+        'minutes' => '1 minuto| :count minutos',
+    ],
+
+    'discussion' => [
+        'new'          => 'Nueva Discusión',
+        'all'          => 'Todas las discusiones',
+        'create'       => 'Crear una discusión',
+        'posted_by'    => 'Publicado por',
+        'head_details' => 'Publicado en categoria',
+
+    ],
+    'response' => [
+        'confirm'     => '¿Estás seguro de que quieres borrar la respuesta?',
+        'yes_confirm' => 'Si, borrar',
+        'no_confirm'  => 'No gracias',
+        'submit'      => 'Enviar respuesta',
+        'update'      => 'Actualizar respuesta',
+    ],
+
+    'editor' => [
+        'title'               => 'Titulo de la discusión',
+        'select'              => 'Selecciona una categoria',
+        'tinymce_placeholder' => 'Agrega el contenido para la discusión aquí...',
+        'select_color_text'   => 'Selecciona un color para la discusión (opcional)',
+    ],
+
+    'email' => [
+        'notify' => 'Notificarme cuando alguien conteste en la discusión',
+    ],
+
+    'auth' => 'Por favor <a href="/:home/login">Inicia sesión</a>
+                o <a href="/:home/register">Regístrate</a>
+                para dejar una respuesta.',
+
+];
