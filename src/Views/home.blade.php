221f395edb8eec2f80a3a03e6f2efaaf014b6c9e
Lucas Leandro Ramos <lucasleandro1204@gmail.com>
2017-05-14 05:02:21 -0300

    drop

--- src/Views/home.blade.php
+++ src/Views/home.blade.php
@@ -64,7 +64,6 @@
 					<button class="btn btn-primary" id="new_discussion_btn"><i class="chatter-new"></i> New {{ Config::get('chatter.titles.discussion') }}</button>
 					<a href="/{{ Config::get('chatter.routes.home') }}"><i class="chatter-bubble"></i> All {{ Config::get('chatter.titles.discussions') }}</a>
 					<ul class="nav nav-pills nav-stacked">
-						<?php $categories = DevDojo\Chatter\Models\Models::category()->all(); ?>
 						@foreach($categories as $category)
 							<li><a href="/{{ Config::get('chatter.routes.home') }}/{{ Config::get('chatter.routes.category') }}/{{ $category->slug }}"><div class="chatter-box" style="background-color:{{ $category->color }}"></div> {{ $category->name }}</a></li>
 						@endforeach
