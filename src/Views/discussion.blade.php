dc15cd4fd31240b9a39ab60cc08b4fe9ee35af73
Chris Breuer <chrisbreuer93@gmail.com>
2017-05-30 13:42:17 -0700

    fix - link to styles correctly

--- src/Views/discussion.blade.php
+++ src/Views/discussion.blade.php
@@ -1,11 +1,11 @@
 @extends(Config::get('chatter.master_file_extend'))
 
 @section(Config::get('chatter.yields.head'))
-	<link href="{{ url('') }}/vendor/devdojo/chatter/assets/css/chatter.css" rel="stylesheet">
+	<link href="{{ url('/vendor/devdojo/chatter/assets/css/chatter.css') }}" rel="stylesheet">
     @if($chatter_editor == 'simplemde')
-        <link href="{{ url('') }}/vendor/devdojo/chatter/assets/css/simplemde.min.css" rel="stylesheet">
+        <link href="{{ url('/vendor/devdojo/chatter/assets/css/simplemde.min.css') }}" rel="stylesheet">
     @elseif($chatter_editor == 'trumbowyg')
-        <link href="{{ url('') }}/vendor/devdojo/chatter/assets/vendor/trumbowyg/ui/trumbowyg.css" rel="stylesheet">
+        <link href="{{ url('/vendor/devdojo/chatter/assets/vendor/trumbowyg/ui/trumbowyg.css') }}" rel="stylesheet">
         <style>
             .trumbowyg-box, .trumbowyg-editor {
                 margin: 0px auto;
