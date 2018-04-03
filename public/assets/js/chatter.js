a5bb778dc0cb09d106b7f5ef0311814a48c26d5b
Matt Libera <me@mattlibera.com>
2018-04-03 12:44:57 -0400

    move $.form definition into doc.ready block

--- public/assets/js/chatter.js
+++ public/assets/js/chatter.js
@@ -25,48 +25,49 @@ $('document').ready(function(){
         // $('#textbox1').val($(this).is(':checked'));        
     });
 
-});
+    // Enable a function $.form to submit a dynamic form
+    jQuery(function($) {
+        $.extend({
+            form: function(url, data, method) {
+                if (method == null) method = 'POST';
+                if (data == null) data = {};
 
-// Enable a function $.form to submit a dynamic form
-jQuery(function($) { $.extend({
-    form: function(url, data, method) {
-        if (method == null) method = 'POST';
-        if (data == null) data = {};
+                var form = $('<form>').attr({
+                    method: method,
+                    action: url
+                }).css({
+                    display: 'none'
+                });
+    
+                var addData = function(name, data) {
+                    if ($.isArray(data)) {
+                        for (var i = 0; i < data.length; i++) {
+                            var value = data[i];
+                            addData(name + '[]', value);
+                        }
+                    } else if (typeof data === 'object') {
+                        for (var key in data) {
+                            if (data.hasOwnProperty(key)) {
+                                addData(name + '[' + key + ']', data[key]);
+                            }
+                        }
+                    } else if (data != null) {
+                        form.append($('<input>').attr({
+                            type: 'hidden',
+                            name: String(name),
+                            value: String(data)
+                        }));
+                    }
+                };
 
-        var form = $('<form>').attr({
-            method: method,
-            action: url
-         }).css({
-            display: 'none'
-         });
-
-        var addData = function(name, data) {
-            if ($.isArray(data)) {
-                for (var i = 0; i < data.length; i++) {
-                    var value = data[i];
-                    addData(name + '[]', value);
-                }
-            } else if (typeof data === 'object') {
                 for (var key in data) {
                     if (data.hasOwnProperty(key)) {
-                        addData(name + '[' + key + ']', data[key]);
+                        addData(key, data[key]);
                     }
                 }
-            } else if (data != null) {
-                form.append($('<input>').attr({
-                  type: 'hidden',
-                  name: String(name),
-                  value: String(data)
-                }));
-            }
-        };
 
-        for (var key in data) {
-            if (data.hasOwnProperty(key)) {
-                addData(key, data[key]);
+                return form.appendTo('body');
             }
-        }
-
-        return form.appendTo('body');
-    }
-}); });
+        });
+    });
+});
