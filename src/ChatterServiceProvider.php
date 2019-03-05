6ec7efc462a9d055eaac606ded74f0a7768a457e
Chris <christhompsontldr@gmail.com>
2019-03-05 13:35:31 -0500

    Load the correct classes.

--- src/ChatterServiceProvider.php
+++ src/ChatterServiceProvider.php
@@ -48,13 +48,13 @@ class ChatterServiceProvider extends ServiceProvider
         /*
          * Register the service provider for the dependency.
          */
-        $this->app->register(\LukeTowers\Purifier\PurifierServiceProvider::class);
+        $this->app->register(\Mews\Purifier\PurifierServiceProvider::class);
 
         /*
          * Create aliases for the dependency.
          */
         $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-        $loader->alias('Purifier', 'LukeTowers\Purifier\Facades\Purifier');
+        $loader->alias('Purifier', 'Mews\Purifier\Facades\Purifier');
 
         $this->loadViewsFrom(__DIR__.'/Views', 'chatter');
     }
