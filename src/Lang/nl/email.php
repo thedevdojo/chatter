bcd838df90a80ddb6df6c71f06156170eb2315ae
Chris <cthompson@wellsmedia.com>
2019-04-18 18:37:45 -0400

    Fixing typo.

--- src/Lang/nl/email.php
+++ src/Lang/nl/email.php
@@ -6,7 +6,7 @@ return [
     'body'            => 'We laten je even weten dat iemand gereageerd heeft op een forum discussie op',
     'view_discussion' => 'Bekijk de discussie',
     'farewell'        => 'Nog een fijne dag gewenst!',
-    'unsuscribe'      => [
+    'unsubscribe'      => [
         'message' => 'Als je geen notificaties meer wilt ontvangen voor deze discussie, wijzig dan je aanmelding onderaan deze pagina :)',
         'action'  => 'Genoeg van deze e-mails?',
         'link'    => 'Afmelden voor deze discussie',
