d23f334a6b827ca9a634ec11ebf5620294597760
Chris <christhompsontldr@gmail.com>
2019-04-18 19:06:55 -0400

    Adding notification subject.

--- src/Lang/en/email.php
+++ src/Lang/en/email.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
+    'subject'         => 'Discussion Reply',
     'preheader'       => 'Just wanted to let you know that someone has responded to a forum post.',
     'greeting'        => 'Hi there,',
     'body'            => 'Just wanted to let you know that someone has responded to a forum post at',
     'view_discussion' => 'View the '.mb_strtolower(trans('chatter::intro.titles.discussion')).'.',
     'farewell'        => 'Have a great day!',
-    'unsubscribe'     => [
+    'unsuscribe'      => [
         'message' => 'If you no longer wish to be notified when someone responds to this form post be sure to uncheck the notification setting at the bottom of the page :)',
         'action'  => 'Don\'t like these emails?',
         'link'    => 'Unsubscribe to this '.mb_strtolower(trans('chatter::intro.titles.discussion')).'.',
--- src/Lang/nl/email.php
+++ src/Lang/nl/email.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
+    'subject'         => 'Discussiereactie',
     'preheader'       => 'We laten je even weten dat iemand gereageerd heeft op een forum discussie.',
     'greeting'        => 'Hallo,',
     'body'            => 'We laten je even weten dat iemand gereageerd heeft op een forum discussie op',
     'view_discussion' => 'Bekijk de discussie',
     'farewell'        => 'Nog een fijne dag gewenst!',
-    'unsubscribe'     => [
+    'unsuscribe'      => [
         'message' => 'Als je geen notificaties meer wilt ontvangen voor deze discussie, wijzig dan je aanmelding onderaan deze pagina :)',
         'action'  => 'Genoeg van deze e-mails?',
         'link'    => 'Afmelden voor deze discussie',
